93c37a6f77c32d9bebd350b267e5298dbc6cab79
Alper280 <79408648+Alper280@users.noreply.github.com>
2024-04-14 12:18:33 +0200

    Update tz.js

--- plugins/tz.js
+++ plugins/tz.js
@@ -2,16 +2,16 @@ import moment from 'moment-timezone';
 
 const handler = async (m, {conn}) => {
 
-  const tzAF = moment().tz('Africa/Casablanca').format('DD/MM HH:mm');
+  const tzEgypt = moment().tz('Africa/Cairo').format('DD/MM HH:mm');
   await conn.sendMessage(m.chat, {text: `\`\`\`
-الوقت الحالي في المغرب هو :
+الوقت الحالي في مصر هو :
 
 
-▢ morocco     : ${tzAF}
+▢ Egypt     : ${tzEgypt}
   ${String.fromCharCode(8206).repeat(850)}
   ▢ instagram.com/noureddine_ouafy`}, {quoted: m});
-  };
+};
 handler.help = ["tz"]
 handler.tags = ["infobot"]
 handler.command = /^(tz)$/i
-  export default handler;
+export default handler;
